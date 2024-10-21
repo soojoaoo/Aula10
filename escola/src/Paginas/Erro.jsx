a6a8ce312c781461127ff2586e8487d4bfb5f161
soojoaoo <gabrieljoao04062008@gmail.com>
2024-10-21 09:37:49 -0300

    21/10/2024

---
+++ escola/src/Paginas/Erro.jsx
@@ -0,0 +1,8 @@
+export default function Erro()
+{
+    return(
+        <div>
+            <h1>Página Não Encontrada</h1>
+        </div>
+    );
+}
---
+++ escola/src/Paginas/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home()
+{
+    return(
+        <div>
+            <h1>Aula 10 - Projeto com MySQL</h1>
+        </div>
+    );
+}
