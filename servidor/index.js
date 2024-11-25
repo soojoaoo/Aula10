4d239feef92bc989f11bdd916cf10acf4cb4af32
soojoaoo <gabrieljoao04062008@gmail.com>
2024-11-25 09:38:27 -0300

    25/11

---
+++ servidor/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'auladb'
+});
+
+// Conectando ao banco de dados
+db.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao conectar ao MySQL:', erro);
+    } else {
+        console.log('Conectando ao MySQL com sucesso!');
+    }
+});
+
+// Rota para cadastrar usuário
+app.post('/alunos', (req, res) => {
+    const {nome, cidade, estado } = req.body;
+
+    const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
+
+    db.query(sql, [nome, cidade, estado], (err, result) => {
+        if (err)
+        {
+            return res.status(500).json({ error: 'Erro ao cadastrar aluno !' });
+        }
+        res.status(201).json({ message: 'Aluno cadastro com sucesso!', id: result.insertId})
+    });
+});
+
+// Rota para consultar todos os usuários
+app.get('/alunos', (req, res) => {
+    const sql = 'SELECT * FROM alunos';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao consultar alunos' });
+        }
+        res.json(results);
+    });
+});
